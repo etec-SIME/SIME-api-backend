70fa1c5da8cf237a514e82855ea5aa92509ad0e7
Raphael567 <raphaelcanuto464@gmail.com>
2025-08-27 20:27:26 -0300

    commit

--- sime.api/src/main/java/br/com/sime/api/entities/escola/ambiente/Tipo_Ambiente.java
+++ sime.api/src/main/java/br/com/sime/api/entities/escola/ambiente/Tipo_Ambiente.java
@@ -1,7 +1,6 @@
 package br.com.sime.api.entities.escola.ambiente;
 
 import br.com.sime.api.entities.chamados.Chamado;
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
