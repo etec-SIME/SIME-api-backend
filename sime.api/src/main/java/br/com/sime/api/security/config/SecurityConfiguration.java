0972c18d595021e0914a9d3887fab7115bc61b26
Raphael567 <raphaelcanuto464@gmail.com>
2025-09-23 11:50:13 -0300

    Feat: fix SecurityConfiguration

--- sime.api/src/main/java/br/com/sime/api/security/config/SecurityConfiguration.java
+++ sime.api/src/main/java/br/com/sime/api/security/config/SecurityConfiguration.java
@@ -77,7 +77,7 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults()) // habilita CORS
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/usuarios/login", "/escolas/login").permitAll()// libera o login
+                        .requestMatchers("/usuarios/login", "/escola/login").permitAll()// libera o login
                         .requestMatchers("/**").hasAnyAuthority("ENTIDADE_ESCOLA", "ENTIDADE_USUARIO")
                         .anyRequest().authenticated() // demais rotas requerem autenticação
                 )
