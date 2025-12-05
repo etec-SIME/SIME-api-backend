48c28f6caee6fe1fdf398b1cda4ebf9012ef21de
Raphael567 <raphaelcanuto464@gmail.com>
2025-12-05 02:17:12 -0300

    Feat: comment EnvLoader

--- src/main/java/br/com/sime/api/config/EnvLoader.java
+++ src/main/java/br/com/sime/api/config/EnvLoader.java
@@ -1,24 +1,24 @@
-package br.com.sime.api.config;
-
-import io.github.cdimascio.dotenv.Dotenv;
-
-public class EnvLoader {
-    public static void loadEnv() {
-        Dotenv dotenv = Dotenv.load();
-
-        setIfPresent("AZURE_STORAGE_CONNECTION_STRING", dotenv);
-        setIfPresent("AZURE_URL", dotenv);
-        setIfPresent("AZURE_USER", dotenv);
-        setIfPresent("AZURE_PASSWORD", dotenv);
-        setIfPresent("JWT_SECRET", dotenv);
-    }
-
-    private static void setIfPresent(String key, Dotenv dotenv) {
-        String value = dotenv.get(key);
-        if (value != null) {
-            System.setProperty(key, value);
-        } else {
-            System.err.println("Variável " + key + " não encontrada no .env");
-        }
-    }
-}
+//package br.com.sime.api.config;
+//
+//import io.github.cdimascio.dotenv.Dotenv;
+//
+//public class EnvLoader {
+//    public static void loadEnv() {
+//        Dotenv dotenv = Dotenv.load();
+//
+//        setIfPresent("AZURE_STORAGE_CONNECTION_STRING", dotenv);
+//        setIfPresent("AZURE_URL", dotenv);
+//        setIfPresent("AZURE_USER", dotenv);
+//        setIfPresent("AZURE_PASSWORD", dotenv);
+//        setIfPresent("JWT_SECRET", dotenv);
+//    }
+//
+//    private static void setIfPresent(String key, Dotenv dotenv) {
+//        String value = dotenv.get(key);
+//        if (value != null) {
+//            System.setProperty(key, value);
+//        } else {
+//            System.err.println("Variável " + key + " não encontrada no .env");
+//        }
+//    }
+//}
